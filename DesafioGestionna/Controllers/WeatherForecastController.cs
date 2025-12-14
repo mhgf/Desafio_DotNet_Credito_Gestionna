01e52f730966875784dec58e583b6d9fce523fd0
Matheus Henrique Gomes Ferreira <mhgf.0208@gmail.com>
2025-12-14 09:02:31 -0300

    Adds initial project structure
    Adds the initial project structure for the application, including necessary files and configurations.
    
    This includes setting up the basic ASP.NET Core project, adding a Dockerfile for containerization, and configuring Swagger for API documentation.

---
+++ DesafioGestionna/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioGestionna.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+   
+}
